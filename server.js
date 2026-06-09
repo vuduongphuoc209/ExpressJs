8a76a64619a53526d34dfba9ecf2716c9755a578
das129600 <phuocvu.hcmc@gmail.com>
2026-06-09 11:53:21 +0700

    middleware

--- server.js
+++ server.js
@@ -3,11 +3,30 @@ const app = express();
 const port = 4001;
 var router1 = require("./router/router");
 
-app.get("/", (req, res) => {
+var dangnhap = true;
+
+var checkAdmin = (req, res, next) => {
+    if (dangnhap) {
+        user.role = "admin";
+        next();
+    } else {
+        res.send("Ban chua dang nhap");
+    }
+};
+
+var checkMiddleware = (req, res, next) => {
+    if (dangnhap) {
+        next();
+    } else {
+        res.send("Ban chua dang nhap");
+    }
+};
+
+app.get("/", checkMiddleware, (req, res, next) => {
     res.send("Server is running in local 4001!");
 });
 
-app.use("/api/v1", router1);
+app.use("/admin/api/v1", checkAdmin, router1);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
